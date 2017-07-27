7b79b378dfd637f48600bbd28f6d94200653e99e
Xie Chang <xiechang@zhongan.io>
2017-07-27 12:39:31 +0800

    delete files

--- lib/psroi_pooling_layer/__init__.py
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------
-# R-FCN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Revised by Minyue Jiang
-# --------------------------------------------------------
